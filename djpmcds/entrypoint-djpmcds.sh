646c4486703e4a105e0ae0425a7e9f15b950b754
Jani V. Anttila <janivaltteri@posteo.net>
2023-09-04 15:15:42 +0300

    application migrations

--- djpmcds/entrypoint-djpmcds.sh
+++ djpmcds/entrypoint-djpmcds.sh
@@ -3,6 +3,8 @@
 echo "starting djpmcds entrypoint script"
 
 python manage.py migrate --noinput
+python manage.py makemigrations mcds --noinput
+python manage.py migrate --noinput
 
 python manage.py shell --command="exec(open('create-users.py').read())"
 
