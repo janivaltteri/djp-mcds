31b6466557f795a9c25b6e15f38cd43a606e6746
Jani V. Anttila <janivaltteri@posteo.net>
2024-02-07 18:09:50 +0200

    added STATIC_ROOT

--- djpmcds/djpmcds/settings.py
+++ djpmcds/djpmcds/settings.py
@@ -105,6 +105,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = "/opt/djpmcds/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
