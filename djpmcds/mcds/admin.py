4f66e9fe7a5c5c515b255c6f17c0a170b61811a2
Jani V. Anttila <janivaltteri@posteo.net>
2024-03-06 15:46:08 +0200

    minor UI fixes

--- djpmcds/mcds/admin.py
+++ djpmcds/mcds/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Project, Download
+from .models import Project, Dataspec, DataspecAssignment, Download
 
 admin.site.register(Project)
+admin.site.register(Dataspec)
+admin.site.register(DataspecAssignment)
 admin.site.register(Download)
-
